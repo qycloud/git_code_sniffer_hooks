c8d449671e637b2f7af8bc517a1434efe6c6677d
robin <474851817@qq.com>
2018-02-13 22:20:09 +0800

    暂时屏蔽检查文件变动逻辑
    容器中不支持新建终端，
    考虑该功能影响范围有限，可以屏蔽。

--- lib/common.py
+++ lib/common.py
@@ -20,21 +20,22 @@ def get_commit_errors(file_type, function):
     return None
 
   #Get files are both in cached zone and modified zone
-  system("git diff --name-status > /tmp/git_hook")
-  modified_files = _get_files(file_type, 2)
-  if modified_files:
-    #Ask user whether add a file in modified zone to commit
-    modified_files = [modified_file for modified_file in modified_files if modified_file in files]
-    stdin = open('/dev/tty')
-    for modified_file in modified_files:
-      print(
-        'File %s has been modified but not in the cached zone, add it into ? [Y|n]' %
-        colored(modified_file, 'red')
-      )
-      if not stdin.readline().strip().lower().startswith('n'):
-        system('git add %s' % modified_file)
-
-    stdin.close()
+  # docker下不支持命令行交互
+  # system("git diff --name-status > /tmp/git_hook")
+  # modified_files = _get_files(file_type, 2)
+  # if modified_files:
+  #   #Ask user whether add a file in modified zone to commit
+  #   modified_files = [modified_file for modified_file in modified_files if modified_file in files]
+  #   stdin = open('/dev/tty')
+  #   for modified_file in modified_files:
+  #     print(
+  #       'File %s has been modified but not in the cached zone, add it into ? [Y|n]' %
+  #       colored(modified_file, 'red')
+  #     )
+  #     if not stdin.readline().strip().lower().startswith('n'):
+  #       system('git add %s' % modified_file)
+  
+  #   stdin.close()
 
   errors = []
   for file_path in files:
